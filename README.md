5c7dde080c79418d0638f77725b7f56039bae3e1
wittystech01 <philipioryem103@gmail.com>
2026-04-06 05:44:40 -0700

    Initial commit for GadgetZone e-commerce project

---
+++ README.md
@@ -0,0 +1,2 @@
+# GadgetZone E-commerce
+This is a full-fledged e-commerce application built with Next.js and MongoDB.
---
+++ models/Product.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose'); const ProductSchema = new mongoose.Schema({ name: String, price: Number, description: String, imageUrl: String, }); module.exports = mongoose.model('Product', ProductSchema);
